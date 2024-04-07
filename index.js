cf3a60d070c369cdacf60b85d4bc81ba7e543879
Mate papava <matepapava123@gmail.com>
2024-04-08 00:12:43 +0400

    adding

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
  
- 
-const x =Math.floor(Math.random() * 10);
-const y =Math.floor(Math.random()  * 10);
-const z =Math.floor(Math.random() * 10);
-const h =Math.floor(Math.random() * 10);
+ function Generating() {
+    const x = Math.floor(Math.random() * 10);
+    const y = Math.floor(Math.random() * 10);
+    const z = Math.floor(Math.random() * 10);
+    const h = Math.floor(Math.random() * 10);
+    
+    return { x, y, z, h };
+}
+
+const { x, y, z, h } = Generating();
+console.log(x, y, z, h);
 
 
-
-
-console.log(x,y,z,h)
